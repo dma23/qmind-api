de7abd2c7a25b773c66001973ffb6e7e626a08c9
Derek Ma <mderek@hotmail.ca>
2021-01-13 00:55:31 -0500

    Added Realtime keyword search

---
+++ app.py
@@ -0,0 +1,26 @@
+import tweepy
+import json
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        print(f"{tweet.user.name}:{tweet.text}")
+
+    def on_error(self, status):
+        print("Error detected")
+
+# Authenticate to Twitter
+
+auth = tweepy.OAuthHandler("lX4x5JrUFU3M9oT61ArQveqBp", "h3QOWQAIINxYzRZy618KHlxTOSAb61wAtiLSYXEgXVRJoTwlTf")
+auth.set_access_token("2210920084-lNgn4Ptey2XveUJ0SLsGCIzneChjOEnwqaiNWdy", "tmEoHlKVvjIcG9Y55r3Io6guPrxnqxMEdO9XsHd6sBcuc")
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
+
+tweets_listener = MyStreamListener(api)
+stream = tweepy.Stream(api.auth, tweets_listener)
+stream.filter(track=["$TSLA", "$NIO"], languages=["en"])
