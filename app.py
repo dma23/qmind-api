d165a0a7e70d6da6eeff9dc655031ed57322424a
Derek <mderek@hotmail.ca>
2021-01-19 18:31:08 -0500

    Editted app.py

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-import tweepy
-import json
-
-class MyStreamListener(tweepy.StreamListener):
-    def __init__(self, api):
-        self.api = api
-        self.me = api.me()
-
-    def on_status(self, tweet):
-        print(f"{tweet.user.name}:{tweet.text}")
-
-    def on_error(self, status):
-        print("Error detected")
+from tweepy import OAuthHandler, Cursor
+from datetime import datetime, timedelta
 
 # Authenticate to Twitter
 
 auth = tweepy.OAuthHandler("lX4x5JrUFU3M9oT61ArQveqBp", "h3QOWQAIINxYzRZy618KHlxTOSAb61wAtiLSYXEgXVRJoTwlTf")
 auth.set_access_token("2210920084-lNgn4Ptey2XveUJ0SLsGCIzneChjOEnwqaiNWdy", "tmEoHlKVvjIcG9Y55r3Io6guPrxnqxMEdO9XsHd6sBcuc")
 
-# Create API object
-api = tweepy.API(auth, wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True)
+authAPI = API(auth)
+collection = []
+count = 0
+user = authAPI.get_user('@StockTwits')
 
-# tweets_listener = MyStreamListener(api)
-# stream = tweepy.Stream(api.auth, tweets_listener)
-# stream.filter(track=["$TSLA", "$NIO"], languages=["en"])
+endDate = datetime.utcnow - timedelta(days=10)
 
-for tweet in tweepy.Cursor(api.search, q='@JimCramer',result_type='popular').items(5):
-    print(tweet)
+for status in Cursor(authAPI.user_timeline, id = user, tweet_mode = 'extended').items(10):
+
+    if "retweeted_status" in dir(status):
+        collection.append(status.retweeted_status.full_text)
+    else:
+        collection.append(status.full_text)
+
+    if status.created_at < endDate:
+        break
+
+print(collection)
+
+"""
+with open('tweets.txt', 'w') as fileWrite:
+    for item in collection:
+        text = item.split("\n")
+        text = ' '.join(text).split()
+        fileWrite.write(' '.join(text))
+        fileWrite.write('\n')
+"""
